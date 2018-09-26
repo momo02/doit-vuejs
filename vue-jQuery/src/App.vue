0e8e78c854bbb9fbca0c35fbe6bcb8022e3671fb
jung <jungha0902@naver.com>
2018-09-26 17:10:17 +0900

    뷰에 jQuery,jQuery UI라이브러리 연동 1)CDN을 이용:index.html파일에 <script>추가. - 날짜 선택기 위젯 컴포넌트화

--- vue-jQuery/src/App.vue
+++ vue-jQuery/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
     App 컴포넌트<br />
-    <input id="calendar" />
+    달력 1 : <DatePicker></DatePicker> <!-- 날짜선택기 표시 -->
+    <br />
+    달력 2 : <DatePicker></DatePicker> <!-- 날짜선택기 표시 -->
   </div>
 </template>
 
 <script>
-export default {
-//App컴포넌트의 인스턴스가 생성되고 나면 바로 created()에 정의된 로직을 실행.
-  // created() {
-  //   console.log($.fn.jquery); //jquery 라이브러리 버전 출력
-  //   console.log($.ui.version); //jquery UI 라이브러리 버전 출력
-  // }
+import DatePicker from './DatePicker.vue' // 날짜 선택기 컴포넌트 로딩
 
-  //jquery 로직을 수행할 때 mounted()를 사용한 이유?
-  //<template>안의 HTML태그에 접근할 수 있는 시점이 뷰의 가상 돔이 화면의 특정 요소에 부착되고 난 시점인 mounted()이기 때문.
-  //mounted의 이전 아리프 사이클 단계인 created에서는 <template>에 정의한 돔 요소에 접근할 수 없음.
-  mounted() {
-    $("#calendar").datepicker();
+export default {
+  //날짜 선택기 컴포넌트 등록
+  components : {
+    // 'DatePicker' : DatePicker 와 동일 => ES6의 객체 리터럴에서 제공하는 약식 문법 사용.
+    DatePicker
   }
 }
 </script>
-
-<style>
-</style>
---
+++ vue-jQuery/src/DatePicker.vue
@@ -0,0 +1,12 @@
+<!-- 날짜 선택기 위젯을 컴포넌트로 분리 -->
+<template>
+  <input class="calendar" />
+</template>
+
+<script>
+export default {
+  mounted() {
+    $(".calendar").datepicker();
+  }
+}
+</script>
