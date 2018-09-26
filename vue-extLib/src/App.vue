fa2a012c0ca92a265f1e77db9a74bae2ecf5a5e2
jung <jungha0902@naver.com>
2018-09-26 17:52:37 +0900

    뷰에 외부 library 연동 (bootstrap,chart library)

---
+++ vue-extLib/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- 부트스트랩 버튼을 추가 -->
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+    <!-- 차트 표시 -->
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Chart from './Chart.vue' //차트 컴포넌트 로딩
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components : { //차트 컴포넌트 등록
+    Chart
+  }
+}
+</script>
---
+++ vue-extLib/src/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="container" style="width:100%; height:400px;"></div>
+</template>
+
+<script>
+export default{
+  mounted() {
+    //출처 : 하이차트 예제 코드 (https://www.highcharts.com/docs/getting-started/your-first-chart)
+      Highcharts.chart('container', {
+         chart: {
+             type: 'bar'
+         },
+         title: {
+             text: 'Fruit Consumption'
+         },
+         xAxis: {
+             categories: ['Apples', 'Bananas', 'Oranges']
+         },
+         yAxis: {
+             title: {
+                 text: 'Fruit eaten'
+             }
+         },
+         series: [{
+             name: 'Jane',
+             data: [1, 0, 4]
+         }, {
+             name: 'John',
+             data: [5, 7, 3]
+         }]
+     });
+  }
+}
+</script>
